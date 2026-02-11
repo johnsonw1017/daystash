e8997f671b6665f0a15e7c461b662238d5baa374
johns <johnsonw1017@gmail.com>
2026-02-11 22:11:59 +1000

    Rename welcome message in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 'use client'
 
-export const Home = () => {
+const Home = () => {
   return (
     <div className="bg-primary flex min-h-screen items-center justify-center">
       <div className="bg-background rounded-lg p-6 text-center shadow-lg">
-        <h1 className="text-foreground text-2xl font-bold">Welcome to Card</h1>
+        <h1 className="text-foreground text-2xl font-bold">
+          Welcome to Daystash
+        </h1>
       </div>
     </div>
   )
