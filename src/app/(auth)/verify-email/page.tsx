524119811aae7ad2f9ee94447ffce56cbf2e7525
johns <johnsonw1017@gmail.com>
2026-05-10 16:21:29 +1000

    fix up verify email page

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,13 +1,19 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
 const VerifyEmailPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <h1 className="text-4xl font-bold">Verify Your Email</h1>
-      <p className="text-xl">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-2xl font-bold">Verify Your Email</h1>
+      <p>
         Thank you for signing up! Please check your email to verify your
         account.
       </p>
+      <Button asChild size="sm">
+        <Link href="/">Return Home</Link>
+      </Button>
     </div>
   )
 }
