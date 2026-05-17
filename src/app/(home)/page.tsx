f5c045ac50d08d97147009b7efb5b5cc41a2810b
johns <johnsonw1017@gmail.com>
2026-05-17 22:19:29 +1000

    add quote and write button

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,21 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import Tree from './_components/Tree'
+import Link from 'next/link'
 
 const Home = () => {
   return (
     <div className="bg-background flex min-h-screen items-center justify-center">
-      <Tree src="/tree.svg" />
+      <div className="flex flex-col items-center gap-4">
+        <Tree src="/tree.svg" />
+        <h2 className="text-center font-serif text-5xl font-bold">
+          Every day leaves something behind.
+        </h2>
+        <Button variant="accent" size="lg" asChild>
+          <Link href="/write">Start Writing</Link>
+        </Button>
+      </div>
     </div>
   )
 }
