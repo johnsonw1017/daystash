44ca9c5d64bd99cd485ba6f54fc516c6018c8780
johns <johnsonw1017@gmail.com>
2026-05-17 22:56:19 +1000

    add a hill

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,9 +6,11 @@ import Link from 'next/link'
 
 const Home = () => {
   return (
-    <div className="bg-background flex min-h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-4">
+    <div className="bg-background flex min-h-screen flex-col items-center overflow-hidden">
+      <div className="mt-16 flex flex-col items-center px-4">
         <Tree src="/tree.svg" />
+      </div>
+      <div className="bg-secondary/35 -mt-4 flex h-full min-h-0 w-full flex-1 flex-col items-center gap-4 px-4 pt-10 md:rounded-t-[40%]">
         <h2 className="text-center font-serif text-5xl font-bold">
           Every day leaves something behind.
         </h2>
