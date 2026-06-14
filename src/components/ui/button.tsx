feb7b14224987603611dd8e561293f3b2bfb21cd
johns <johnsonw1017@gmail.com>
2026-06-14 12:29:59 +1000

    fix some dark mode buttons text colour

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,8 @@ const buttonVariants = cva(
         ghost:
           'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
-        accent: 'bg-accent text-accent-foreground hover:bg-accent/80',
+        accent:
+          'bg-accent text-accent-foreground dark:text-secondary-foreground hover:bg-accent/80',
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
