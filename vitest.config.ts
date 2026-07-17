3e7b32991203bf3621664fbd748076ae71a2fed2
johns <johnsonw1017@gmail.com>
2026-07-17 13:37:03 +1000

    Address PR coverage recommendations

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,12 @@ export default defineConfig({
       exclude: ['src/**/*.test.{ts,tsx}', 'src/**/*.d.ts'],
       include: ['src/**/*.{ts,tsx}'],
       provider: 'v8',
+      thresholds: {
+        branches: 29,
+        functions: 39,
+        lines: 37,
+        statements: 36,
+      },
     },
     environment: 'jsdom',
     globals: true,
