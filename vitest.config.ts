21d13640e17fb63671e0e335fba3736b4d22e77b
johns <johnsonw1017@gmail.com>
2026-07-17 10:40:03 +1000

    Add Vitest coverage reporting

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,12 @@ export default defineConfig({
     tsconfigPaths: true,
   },
   test: {
+    coverage: {
+      all: true,
+      exclude: ['src/**/*.test.{ts,tsx}', 'src/**/*.d.ts'],
+      include: ['src/**/*.{ts,tsx}'],
+      provider: 'v8',
+    },
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./vitest.setup.ts'],
